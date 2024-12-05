2ec28eac7068d8ea4cccb68ab0650bf5d98bbfd8
= <=>
2024-12-05 09:42:27 +0100

    New emotions show up underneath old emotions

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ from openai import OpenAI
 import pandas as pd
 import numpy as np
 import faiss
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_caching import Cache
 
 import utils as utils
 
 app = Flask(__name__)
+app.secret_key = 'oooooooohsoseeeeecreeeeeeeet'
 
 # Set up OpenAI API access
 api_key = os.environ.get('OPENAI_API_KEY')
@@ -25,26 +26,26 @@ df_embeddings = pd.read_pickle('Data/Processed/' + dataset_embeddings + '.pkl')
 faiss_index = utils.get_faiss_index(df_embeddings)
 emotion_list = df_embeddings['Emotion'].tolist()
 
-# For storing previous emotions recommended
-# (Somehow this needs to be a cache but others not??)
-cache = Cache(app, config={
-    'CACHE_TYPE': 'simple'
-})
+# TO-DO: set number of emotions to show
+# num_emotions = 3
 
-@app.route('/E*star', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
-@app.route('/E*star', methods=['POST'])
+@app.route('/starting', methods=['POST'])
 def first_pass():
     
     # Get user_input
     user_input = request.form.get('user_input')
+    if user_input[-1] != '.': 
+        # Ensure user inputted sentence ends with a period
+        user_input += '.'
     
-    # Initialise cache
-    cache.set('previous_emotions', [])
-    cache.set('chosen_emotions', [])
-    cache.set('original_user_input', user_input)
+    # Initialise session variables
+    session['previous_emotions'] = []
+    session['chosen_emotions'] = []
+    session['original_user_input'] = user_input
 
     # Get recommended_emotions
     recommended_emotions = utils.find_relevant_emotions(
@@ -55,44 +56,44 @@ def first_pass():
     )
     
     # Append recommended_emotions to previous_emotions
-    previous_emotions = cache.get('previous_emotions')
+    previous_emotions = session['previous_emotions']
     for emotion in recommended_emotions:
         previous_emotions.append(emotion)
-    cache.set('previous_emotions', previous_emotions)
     
     print(f"User input: {user_input}\nRecommended emotions: {recommended_emotions}\nPrevious emotions: {previous_emotions}")
 
-    return render_template('results.html', emotions=recommended_emotions, user_input=user_input, current_prompt=user_input, original_user_input=user_input)
+    return render_template('results.html', 
+                           emotions=recommended_emotions, 
+                           user_input=user_input, 
+                           previous_sets=[],
+                           current_prompt=user_input, 
+                           original_user_input=user_input)
 
 
-@app.route('/E*star', methods=['POST'])
+@app.route('/wandering', methods=['POST'])
 def get_emotions():
     
     # Get user_input
-    original_user_input = cache.get('original_user_input')
+    original_user_input = session['original_user_input']
     user_input = request.form.get('user_input')
     
     # Get previous emotions
-    previous_emotions = cache.get('previous_emotions')
+    previous_emotions = session['previous_emotions']
+    
+    # Create sets of previous emotions (groups of 3)
+    previous_sets = [previous_emotions[i:i+3] for i in range(0, len(previous_emotions), 3)]
     
     # Get chosen_emotion and add it to list of chosen emotions
-    # Keeping track needed (?) to show the path through the chosen emotions
-    chosen_emotions = cache.get('chosen_emotions')
+    chosen_emotions = session['chosen_emotions']
     chosen_emotion = request.form.get('chosen_emotion')
-    # Not the best way to tackle it.....
-    if chosen_emotion:
-        print('In chosen_emotion loop')
-        chosen_emotions.append(chosen_emotion)
-    cache.set('chosen_emotions', chosen_emotions)
+    chosen_emotions.append(chosen_emotion)
     
     # Find out the other emotions
-    if chosen_emotion:
-        other_emotions = cache.get('previous_emotions')[-3:]
-        other_emotions.remove(chosen_emotion)
+    other_emotions = previous_emotions[-3:].copy()
+    other_emotions.remove(chosen_emotion)
     
-    # If not in the first loop, append chosen emotion and other emotions to user input
-    if chosen_emotion:
-        user_input += f" I feel that '{chosen_emotion}' describes my experience better than '{other_emotions[0]}' and '{other_emotions[1]}'."
+    # Append chosen emotion and other emotions to user input
+    user_input += f" I feel that '{chosen_emotion}' describes my experience better than '{other_emotions[0]}' and '{other_emotions[1]}'."
     
     # Get recommended_emotions
     recommended_emotions = utils.find_relevant_emotions(
@@ -106,15 +107,28 @@ def get_emotions():
     # Append recommended_emotions to previous_emotions
     for emotion in recommended_emotions:
         previous_emotions.append(emotion)
-    cache.set('previous_emotions', previous_emotions)
     
     print(f"User input: {user_input}\nChosen emotion: {chosen_emotion}\nRecommended emotions: {recommended_emotions}\nPrevious emotions: {previous_emotions}")
     
+    # Session has been modified at this point
+    session.modified = True
+    
     # If no new emotions, inform the user
     if not recommended_emotions:
-        return render_template('results.html', emotions=[], message="No new emotions found.", user_input=user_input, recommended_emotions=recommended_emotions, previous_emotions=previous_emotions, chosen_emotion=chosen_emotion, current_prompt=user_input)
+        return render_template('results.html', 
+                                emotions=recommended_emotions, 
+                                message="No new emotions found",
+                                user_input=user_input, 
+                                previous_sets = previous_sets,
+                                chosen_emotion=chosen_emotion, 
+                                original_user_input=original_user_input)
 
-    return render_template('results.html', emotions=recommended_emotions, user_input=user_input, chosen_emotion=chosen_emotion, original_user_input=original_user_input)
+    return render_template('results.html', 
+                           emotions=recommended_emotions, 
+                           user_input=user_input, 
+                           previous_sets = previous_sets,
+                           chosen_emotion=chosen_emotion, 
+                           original_user_input=original_user_input)
 
 @app.route('/finish', methods=['POST'])
 def finish():
